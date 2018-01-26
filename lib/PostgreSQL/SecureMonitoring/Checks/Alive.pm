899f180562ddfe7d204940d5fde8191ee8291f38
Alvar C.H. Freude <github_ok@alvar.a-blast.org>
2018-01-26 09:28:27 +0100

    Add a TODO

--- lib/PostgreSQL/SecureMonitoring/Checks/Alive.pm
+++ lib/PostgreSQL/SecureMonitoring/Checks/Alive.pm
@@ -13,6 +13,9 @@ package PostgreSQL::SecureMonitoring::Checks::Alive;
 This is a minimalistic Posemo check. It creates a SQL for a simple function, 
 which just returns true. So, if the server is alive, then it returns true.
 
+=head2 TODO
+
+Catch DBI connection error and return 0 (not alive) instead of error!
 
 =cut
 
