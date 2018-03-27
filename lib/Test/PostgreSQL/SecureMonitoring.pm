1996c26a60d1a59b1c55e2fce9b3a2ef119a6a7c
Alvar C.H. Freude <github_ok@alvar.a-blast.org>
2018-03-27 18:57:29 +0200

    new check can take parameters
    A new option for result_ok introduced: parameters for the new check.
    warning_ok and critical_ok will now be exported.

--- lib/Test/PostgreSQL/SecureMonitoring.pm
+++ lib/Test/PostgreSQL/SecureMonitoring.pm
@@ -62,7 +62,7 @@ give more then one test result.
 
 use base qw(Exporter);
 our @EXPORT
-   = qw( result_ok no_warning_ok no_critical_ok no_error_ok error_ok name_is result_type_is row_type_is result_unit_is result_is result_cmp);
+   = qw( result_ok no_warning_ok no_critical_ok warning_ok critical_ok no_error_ok error_ok name_is result_type_is row_type_is result_unit_is result_is result_cmp);
 
 
 # use parent 'Test::Builder::Module';
@@ -84,10 +84,11 @@ Returns the result object for check $check in the $clustername cluster.
 =cut
 
 
-sub result_ok($;$$)
+sub result_ok($;$$$)
    {
    my $checkname   = shift;
    my $clustername = shift;
+   my $params      = shift;
    my $message     = shift // ( "Result for $checkname on cluster " . ( $clustername // "unnamed" ) );
 
    my $tps_conf = pg_read_conf_ok( $clustername, "Read conf (for $message)" );
@@ -115,7 +116,7 @@ sub result_ok($;$$)
       skip "Got no Object for $message", 2 unless $app;
 
       # Get Check Object
-      lives_ok( sub { $check = $app->new_check($checkname) }, "Check Object (for $message)" );
+      lives_ok( sub { $check = $app->new_check( $checkname, $params ) }, "Check Object (for $message)" );
 
       skip "Got no check object for $message", 1 unless $check;
 
@@ -128,7 +129,7 @@ sub result_ok($;$$)
    ok( ref $result eq "HASH", "got Result for $message" ) or diag "Result is no hashref: " . Dumper($result);
 
    return $result;
-   } ## end sub result_ok($;$$)
+   } ## end sub result_ok($;$$$)
 
 
 
