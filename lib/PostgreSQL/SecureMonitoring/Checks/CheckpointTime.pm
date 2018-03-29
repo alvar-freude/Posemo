e4037e394d65668b311a8447ed96d35ca7e86b79
Alvar C.H. Freude <github_ok@alvar.a-blast.org>
2018-03-29 20:17:50 +0200

    New Check: CheckpointTime
    this new check gives statistics about checkpoint_write_time and checkpoint_sync_time

---
+++ lib/PostgreSQL/SecureMonitoring/Checks/CheckpointTime.pm
@@ -0,0 +1,53 @@
+package PostgreSQL::SecureMonitoring::Checks::CheckpointTime;
+
+=head1 NAME
+
+ PostgreSQL::SecureMonitoring::Checks::CheckpointTime -- statistics about checkpoint write/sync duration
+
+=head1 SYNOPSIS
+
+This check has no extra config options beside the defaults.
+
+
+=head1 DESCRIPTION
+
+This check returns the checkpoint write and sync times for the complete cluster.
+
+
+=head2 SQL/Result
+
+The SQL generates a result like this:
+
+  write_time | sync_time 
+ ------------+------------
+  2264947613 |  2152766
+
+
+=cut
+
+
+use PostgreSQL::SecureMonitoring::ChecksHelper;
+extends "PostgreSQL::SecureMonitoring::Checks";
+
+check_has
+   description       => "Checkpoint write and sync duration.",
+   result_type       => "double precision",
+   result_unit       => "milliseconds",
+   result_is_counter => 1,
+   graph_type        => "stacked_area",
+
+   # complex return type
+   return_type => q{
+      write_time    double precision,
+      sync_time     double precision
+      },
+
+   code => q{
+      SELECT checkpoint_write_time, checkpoint_sync_time FROM pg_stat_bgwriter;
+      };
+
+
+1;
+
+
+
---
+++ t/823-checkpoin-time.t
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Test::PostgreSQL::SecureMonitoring;
+
+use Test::Deep;
+
+
+my $result = result_ok "CheckpointTime", "test";
+
+no_warning_ok $result;
+no_critical_ok $result;
+no_error_ok $result;
+
+name_is $result,        "Checkpoint Time";
+result_type_is $result, "double precision";
+row_type_is $result,    "list";
+result_unit_is $result, "milliseconds";
+result_is_counter $result;
+
+
+is scalar @{ $result->{result} }, 2, "Result has two columns";
+
+cmp_ok $result->{result}[0], '>', 0, "write_time is more then 0 milliseconds";
+
+# the sync time is usually 0, but don't check, because it depends on environment!
+# cmp_ok $result->{result}[1], '>', 0, "sync_time is more then 0 milliseconds";
+
+
+done_testing();
+
